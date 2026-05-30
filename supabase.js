5dcc6a889490fc456726f8bc43a20c9ab84714ff
Jagathish025-OS <jaga31699@gmail.com>
2026-05-30 23:34:54 +0530

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+  "https://lhsptgfeoojuvavsqdas.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_cdCtff0oXM7b-Bv4iEJ7Kg_tMEnhr38";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
