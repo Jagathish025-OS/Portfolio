67432fbd4ddffd16317e15efbde032e1a4fa39ee
Jagathish025-OS <jaga31699@gmail.com>
2026-05-30 23:41:07 +0530

    auth.js

--- auth.js
+++ auth.js
@@ -42,3 +42,27 @@ async function signUp() {
 
   location.reload();
 }
+async function login() {
+
+  const email =
+    document.getElementById("loginEmail").value;
+
+  const password =
+    document.getElementById("loginPassword").value;
+
+  const { data, error } =
+    await supabaseClient.auth.signInWithPassword({
+      email,
+      password
+    });
+
+  if (error) {
+
+    alert(error.message);
+
+    return;
+  }
+
+  window.location.href =
+    "dashboard.html";
+}
